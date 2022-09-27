1e4a0df41d410d826f15f312153525d880a74cd2
Țurcan Stanislav <sturcan@sapec.md>
2022-09-27 17:13:41 +0300

    update

---
+++ CalculatorApp/Input/InputOperator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    internal class InputOperator
+    {
+        protected static bool validOperator = false;
+        protected static char _operator;
+
+        public static char ReadOperator()
+        {
+            Console.WriteLine("                  Please, choose the operation by inputting its corresponding operator");
+            while (!validOperator)
+            {
+                Console.WriteLine("Operators:      +(addition)   |   -(subtraction)   |   /(division)   |   *(multiplication)");
+                Console.Write("Operator: ");
+                char op = Console.ReadKey().KeyChar;
+
+                UserInterface.ShowMainInterface();
+
+                bool isShortcut = NextAction.CheckIfShortcut(op);
+                if (isShortcut == true) continue;
+
+                Console.WriteLine();
+
+                if (CheckOperator(op) == true)
+                {
+                    _operator = op;
+                    break;
+                }
+            }
+            validOperator = false;
+            return _operator;
+        }
+
+        protected static bool CheckOperator(char _operator)
+        {
+            switch (_operator)
+            {
+                case '+':
+                    {
+                        validOperator = true;
+                        break;
+                    }
+                case '-':
+                    {
+                        validOperator = true;
+                        break;
+                    }
+                case '*':
+                    {
+                        validOperator = true;
+                        break;
+                    }
+                case '/':
+                    {
+                        validOperator = true;
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("An unexistent operator. Please, try again.");
+                        break;
+                    }
+            }
+
+            return validOperator;
+        }
+
+    }   
+}
--- CalculatorApp/Launch.cs
+++ CalculatorApp/Launch.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace CalculatorApp
 {
     class Launch
     {
+        const int MF_BYCOMMAND = 0x00000000;
+        const int SC_MINIMIZE = 0xF020;
+        const int SC_MAXIMIZE = 0xF030;
+        const int SC_SIZE = 0xF000;
+
         protected static char _operator;
         protected static decimal operand1;
         protected static decimal operand2;
         protected static decimal result;
-        internal static bool calculationFinished; 
+        internal static bool calculationFinished;
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
 
         public static void Execute()
         {
@@ -16,17 +31,37 @@ namespace CalculatorApp
             Console.Clear();
             UserInterface.ShowMainInterface();
             _operator = InputOperator.ReadOperator();
-            operand1 = InputOperand.Read();
-            operand2 = InputOperand.Read();
+            operand1 = InputOperand.Read("first");
+            operand2 = InputOperand.Read("second");
 
             result = Operation.Calculate(_operator, operand1, operand2);
             UserInterface.ShowResult(_operator, operand1, operand2, result);
             calculationFinished = true;
             NextAction.ReadKey();
         }
+        public static decimal FirstOperand()
+        {
+            return operand1;
+        }
+        public static decimal SecondOperand()
+        {
+            return operand2;
+        }
+        public static char Operator()
+        {
+            return _operator;
+        }
+        protected static void FixedWindow()
+        {
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MINIMIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MAXIMIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_SIZE, MF_BYCOMMAND);
+        }
 
         static void Main(string[] args)
         {
+            Console.SetWindowSize(122, 30);
+            FixedWindow();
             Execute();
         }
     }
